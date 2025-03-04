99fb59d7db1cf84b8a48de976d74ed4565d06fda
Robert Collins <rcollins@redmug.co.uk>
2025-03-04 22:15:42 +0000

    Removed some console logging

--- utils/writeSamplePage.js
+++ utils/writeSamplePage.js
@@ -12,12 +12,12 @@ export async function writePage(type, content, rootPath, name, extension) {
 
   let file = name + '.' + extension
   fullPath = path.join(rootPath, 'src', file)
-  // console.log('🚀 ~ index4.js:280 ~ writePage ~ fullPath:', fullPath)
+
   try {
     fs.writeFileSync(fullPath, content, { encoding: 'utf8' }) // Specify UTF-8 encoding
-    if (type === 'layouts' || type === 'partials') {
-      console.log('File written type = ', type, ' fullPath = ', fullPath)
-    }
+    // if (type === 'layouts' || type === 'partials') {
+    //   console.log('File written type = ', type, ' fullPath = ', fullPath)
+    // }
   } catch (err) {
     console.error('Error writing file of type = ' + type + ' and file name= ' + fullPath + ' \n', err)
   }
